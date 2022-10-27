3eb33118468a46dbb68681aed024a0be1cd30791
samimsarkar <samimsarkar447@gmail.com>
2022-10-28 01:06:46 +0600

    Registration  page completed successfully

--- src/pages/Faq/Faq.jsx
+++ src/pages/Faq/Faq.jsx
@@ -1,130 +1,130 @@
 import react from 'react';
 
-// const Faq = () => {
-//     return (
-//         <div className='bg-slate-400 py-32'>
-//             <h1 className='text-3xl font-bold text-center text-black-400'>FAQ Question and Answering Part</h1>
-//             <div className="space-y-4 mx-5 mt-12">
-//                 <details className="group border-l-4 border-green-500 bg-gray-50 p-6" open>
-//                     <summary className="flex cursor-pointer items-center justify-between">
-//                         <h5 className="text-lg font-medium text-gray-900">
-//                             what is cors?
-//                         </h5>
+const Faq = () => {
+    return (
+        <div className='bg-slate-400 py-32'>
+            <h1 className='text-3xl font-bold text-center text-black-400'>FAQ Question and Answering Part</h1>
+            <div className="space-y-4 mx-5 mt-12">
+                <details className="group border-l-4 border-green-500 bg-gray-50 p-6" open>
+                    <summary className="flex cursor-pointer items-center justify-between">
+                        <h5 className="text-lg font-medium text-gray-900">
+                            what is cors?
+                        </h5>
 
-//                         <span
-//                             className="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3"
-//                         >
-//                             <svg
-//                                 xmlns="http://www.w3.org/2000/svg"
-//                                 className="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
-//                                 viewBox="0 0 20 20"
-//                                 fill="currentColor"
-//                             >
-//                                 <path
-//                                     fill-rule="evenodd"
-//                                     d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-//                                     clip-rule="evenodd"
-//                                 />
-//                             </svg>
-//                         </span>
-//                     </summary>
+                        <span
+                            className="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                        </span>
+                    </summary>
 
-//                     <p className="mt-4 leading-relaxed text-gray-700">
-//                         Cross-origin resource sharing is a mechanism that allows restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served. A web page may freely embed cross-origin images, stylesheets, scripts, iframes, and videos
-//                     </p>
-//                 </details>
+                    <p className="mt-4 leading-relaxed text-gray-700">
+                        Cross-origin resource sharing is a mechanism that allows restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served. A web page may freely embed cross-origin images, stylesheets, scripts, iframes, and videos
+                    </p>
+                </details>
 
-//                 <details className="group border-l-4 border-green-500 bg-gray-50 p-6">
-//                     <summary className="flex cursor-pointer items-center justify-between">
-//                         <h5 className="text-lg font-medium text-gray-900">
-//                             Why are you using firebase? What other options do you have to implement authentication?
-//                         </h5>
+                <details className="group border-l-4 border-green-500 bg-gray-50 p-6">
+                    <summary className="flex cursor-pointer items-center justify-between">
+                        <h5 className="text-lg font-medium text-gray-900">
+                            Why are you using firebase? What other options do you have to implement authentication?
+                        </h5>
 
-//                         <span
-//                             className="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3"
-//                         >
-//                             <svg
-//                                 xmlns="http://www.w3.org/2000/svg"
-//                                 className="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
-//                                 viewBox="0 0 20 20"
-//                                 fill="currentColor"
-//                             >
-//                                 <path
-//                                     fill-rule="evenodd"
-//                                     d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-//                                     clip-rule="evenodd"
-//                                 />
-//                             </svg>
-//                         </span>
-//                     </summary>
+                        <span
+                            className="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                        </span>
+                    </summary>
 
-//                     <p className="mt-4 leading-relaxed text-gray-700">
-//                         The Firebase Realtime Database lets you build rich, collaborative applications by allowing secure access to the database directly from client-side code. Data is persisted locally, and even while offline, realtime events continue to fire, giving the end user a responsive experience.
+                    <p className="mt-4 leading-relaxed text-gray-700">
+                        The Firebase Realtime Database lets you build rich, collaborative applications by allowing secure access to the database directly from client-side code. Data is persisted locally, and even while offline, realtime events continue to fire, giving the end user a responsive experience.
 
-//                         Usually, authentication by a server entails the use of a user name and password. Other ways to authenticate can be through cards, retina scans, voice recognition, and fingerprints.
-//                     </p>
-//                 </details>
+                        Usually, authentication by a server entails the use of a user name and password. Other ways to authenticate can be through cards, retina scans, voice recognition, and fingerprints.
+                    </p>
+                </details>
 
-//                 <details className="group border-l-4 border-green-500 bg-gray-50 p-6">
-//                     <summary className="flex cursor-pointer items-center justify-between">
-//                         <h5 className="text-lg font-medium text-gray-900">
-//                             How does the private route work?
-//                         </h5>
+                <details className="group border-l-4 border-green-500 bg-gray-50 p-6">
+                    <summary className="flex cursor-pointer items-center justify-between">
+                        <h5 className="text-lg font-medium text-gray-900">
+                            How does the private route work?
+                        </h5>
 
-//                         <span
-//                             className="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3"
-//                         >
-//                             <svg
-//                                 xmlns="http://www.w3.org/2000/svg"
-//                                 className="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
-//                                 viewBox="0 0 20 20"
-//                                 fill="currentColor"
-//                             >
-//                                 <path
-//                                     fill-rule="evenodd"
-//                                     d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-//                                     clip-rule="evenodd"
-//                                 />
-//                             </svg>
-//                         </span>
-//                     </summary>
+                        <span
+                            className="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                        </span>
+                    </summary>
 
-//                     <p className="mt-4 leading-relaxed text-gray-700">
-//                         The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in)
-//                     </p>
-//                 </details>
-//                 <details className="group border-l-4 border-green-500 bg-gray-50 p-6">
-//                     <summary className="flex cursor-pointer items-center justify-between">
-//                         <h5 className="text-lg font-medium text-gray-900">
-//                             What is Node? How does Node work?
-//                         </h5>
+                    <p className="mt-4 leading-relaxed text-gray-700">
+                        The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in)
+                    </p>
+                </details>
+                <details className="group border-l-4 border-green-500 bg-gray-50 p-6">
+                    <summary className="flex cursor-pointer items-center justify-between">
+                        <h5 className="text-lg font-medium text-gray-900">
+                            What is Node? How does Node work?
+                        </h5>
 
-//                         <span
-//                             className="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3"
-//                         >
-//                             <svg
-//                                 xmlns="http://www.w3.org/2000/svg"
-//                                 className="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
-//                                 viewBox="0 0 20 20"
-//                                 fill="currentColor"
-//                             >
-//                                 <path
-//                                     fill-rule="evenodd"
-//                                     d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-//                                     clip-rule="evenodd"
-//                                 />
-//                             </svg>
-//                         </span>
-//                     </summary>
+                        <span
+                            className="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                        </span>
+                    </summary>
 
-//                     <p className="mt-4 leading-relaxed text-gray-700">
-//                         Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on a JavaScript Engine and executes JavaScript code outside a web browser, which was designed to build scalable network applications.
-//                         It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.
-//                     </p>
-//                 </details>
-//             </div>
-//         </div>
-//     )
-// }
+                    <p className="mt-4 leading-relaxed text-gray-700">
+                        Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on a JavaScript Engine and executes JavaScript code outside a web browser, which was designed to build scalable network applications.
+                        It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.
+                    </p>
+                </details>
+            </div>
+        </div>
+    )
+}
 
 export default Faq; 
