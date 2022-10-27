b9d7592857274492b3e7f3e3246e5d8850597d6f
samimsarkar <samimsarkar447@gmail.com>
2022-10-28 00:57:25 +0600

    Header part done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,14 +3,14 @@ import { Outlet } from 'react-router-dom';
 import Footer from '../pages/shared/Footer/Footer';
 import Header from '../pages/shared/Header/Header';
 
-const Main = () => {
-    return (
-        <>
-            <Header></Header>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </>
-    );
-};
+// const Main = () => {
+//     return (
+//         <>
+//             <Header></Header>
+//             <Outlet></Outlet>
+//             <Footer></Footer>
+//         </>
+//     );
+// };
 
-export default Main;
+// export default Main;
